01d2f6657de34322ee973fa452440aea8e28ee42
Sahar Cohen <saharcohen456@gmail.com>
2024-06-17 08:22:23 -0400

    init

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['text']
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -22,9 +30,10 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
-
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
 admin.site.register(Instructor)
 admin.site.register(Learner)
--- onlinecourse/apps.py
+++ onlinecourse/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class OnlinecourseConfig(AppConfig):
     name = 'onlinecourse'
+    default_auto_field = 'django.db.models.BigAutoField'  # Set the default_auto_field attribute
